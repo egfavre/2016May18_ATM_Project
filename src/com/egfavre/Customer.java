7709c54f62038376e35bb74720f2241e04e0ad57
Liz Favre <egfavre@gmail.com>
2016-05-20 14:19:56 -0400

    2016May20 ATM loop assignment

--- src/com/egfavre/Customer.java
+++ src/com/egfavre/Customer.java
@@ -4,11 +4,11 @@ package com.egfavre;
  * Created by user on 5/18/16.
  */
 public class Customer {
-    String name;
+    static String name;
     String transact;
     int transNum;
 
-    public void enterName() throws Exception {
+    public static void enterName() throws Exception {
         System.out.println("Please enter you name.");
 
         name = Main.scanner.nextLine();
@@ -18,7 +18,23 @@ public class Customer {
         }
     }
 
-    public static void enterTransaction() throws Exception {
+    public static double createAccount(Double customerBalance, String customerName, Double customerDeposit) {
+        while (customerBalance == null) {
+            System.out.println("You do not currently have an account.  Would you like to open a new account today?[Y/N]");
+            String newAccount = Main.scanner.nextLine();
+            if (newAccount.equalsIgnoreCase("Y")) {
+                System.out.println("How much would you like to deposit today? [example: 200.00]");
+                customerDeposit = Main.scanner.nextDouble();
+            }
+            else {
+                customerName = "end";
+            }
+            return customerDeposit;
+        }
+        return customerDeposit;
+    }
+
+    public static void enterTransaction(Double customerBalance) throws Exception {
 
         System.out.println("Select the number of the transaction would you like to complete today?");
         System.out.println("1. Check Blance");
@@ -29,16 +45,25 @@ public class Customer {
 
         int transNum = Integer.valueOf(transact);
 
+        //While loop to send back to start if canceled- use a break statement
+
         if (transNum == 1) {
             System.out.println("You have choosen "  + transact + "'check balance'.");
-            System.out.println("Your Balance is $100.00");
+
+            System.out.println("Your Balance is $" + customerBalance);
         }
         else if (transNum == 3) {
             System.out.println("You have choosen " + transact + "'cancel.'");
             System.out.println("Thank you and come again!");
+            //end program, loop to start with break statement
         }
         else if (transNum == 2) {
             System.out.println("You have choosen " + transact + "'withdrawl'.");
+
+            //show account balance
+            //how much to withdraw
+            //is want > have; Yes: not valid, loop to transaction type; No: take money & loop to transaction type
+
             System.out.println("How much would you like to withdraw?");
 
             String withdrawlAmount = Main.scanner.nextLine();
@@ -48,10 +73,14 @@ public class Customer {
             if (100 < amount){
                 throw new Exception("Cannot complete transaction");
             }
+
             else {
                 System.out.println("Please take your money.");
             }
         }
+
+        //add option 4- remove account
+
         else {
             throw new Exception("invalid transaction choice");
         }
--- src/com/egfavre/Main.java
+++ src/com/egfavre/Main.java
@@ -1,5 +1,6 @@
 package com.egfavre;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -7,17 +8,54 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
+
+        Double customerDeposit = 0.0;
+
+        Double customerBalance;
+
+        int transNum = 0;
+
+        HashMap<String, Double> accounts = new HashMap<>();
+
+        //Set up HashMap
+        accounts.put("Abigail", 5999.35);
+        accounts.put("Benjamin", 76459.83);
+        accounts.put("Catherine", 85.45);
+        accounts.put("David", 2.02);
+        accounts.put("Eugene", 1.75);
+        accounts.put("Fisher", 345.23);
+        accounts.put("Georgia", 25375.34);
+        accounts.put("Harriette", 43.45);
+        accounts.put("Ingrid", 234.34);
+        accounts.put("Jefferson", 234.23);
+        accounts.put("Kevin", 3453.23);
+        accounts.put("Laura", 234.56);
+        accounts.put("Monica", 9834.34);
+        accounts.put("Newman", 234.34);
+        accounts.put("Owen", 9834539745829374.34);
+
+        //Test accounts works:
+        //Double test = accounts.get("Marcus");
+        //System.out.println(test);
+        //result = "null"
+
+
+        //Start: Welcome and collect name:
         System.out.println("Welcome to the ATM.");
 
         Customer customer = new Customer();
-
         customer.enterName();
+        String customerName = Customer.name;
+        customerBalance = accounts.get(customerName);
 
-        System.out.println("Welcome, " + customer.name);
+        Customer.createAccount(customerBalance, customerName, customerDeposit);
+        accounts.put(customerName, customerBalance);
 
-        customer.enterTransaction();
+        while (customerName != "end" && transNum != 3) {
+            System.out.println("Welcome, " + customer.name);
+            customer.enterTransaction();
+        }
 
         System.out.println("Have a nice day!");
-
     }
 }
